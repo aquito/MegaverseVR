171750b725087c672fd99ef3ddf7a2744cdc62a1
Aki <aki@gamefutures.com>
2018-07-14 23:00:19 +0100

    stage menu 1st version
    - menu toggled on/off with top button
    - 4 buttons that simply turn different things off
    - lighting tweaks

---
+++ MegaverseVRstage/Assets/Scripts/MenuToggle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class MenuToggle : MonoBehaviour {
+
+	public VRTK_ControllerEvents controllerEvents;
+	public GameObject menu;
+
+	bool menuState = false;
+	/// <summary>
+	/// This function is called when the object becomes enabled and active.
+	/// </summary>
+	void OnEnable()
+	{
+
+		controllerEvents.ButtonTwoReleased += ControllerEvents_ButtonTwoReleased;
+
+	}
+
+	/// <summary>
+	/// This function is called when the behaviour becomes disabled or inactive.
+	/// </summary>
+	void OnDisable()
+	{
+		controllerEvents.ButtonTwoReleased -= ControllerEvents_ButtonTwoReleased;
+	}
+
+	private void ControllerEvents_ButtonTwoReleased(object sender, ControllerInteractionEventArgs e)
+	{
+		 menuState = !menuState;
+		 menu.SetActive(menuState);
+	}
+
+}
