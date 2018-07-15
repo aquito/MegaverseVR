f2937eb122c34435156e4bea9dfa22aa3e868eb6
Aki <aki@gamefutures.com>
2018-07-15 21:43:50 +0100

    start on scene management

---
+++ MegaverseVRstage/Assets/Scripts/StageManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageManager : MonoBehaviour {
+
+	private string _currentLevelName = string.Empty;
+
+
+	private void Start()
+	{
+		DontDestroyOnLoad(gameObject);
+
+		LoadLevel("SampleScene");
+	}
+	
+	void OnLoadOperationComplete(AsyncOperation ao)
+	{
+		Debug.Log("Scene load complete");
+	}
+
+	void OnUnloadOperationComplete(AsyncOperation ao)
+	{
+		Debug.Log("Scene unload complete");
+	}
+
+	public void LoadLevel(string levelName)
+	{
+		AsyncOperation ao = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive); // additive scene load so that this manager remains in memory
+		
+		if(ao == null)
+		{
+			Debug.LogError("[StageManager] unable to load level " + levelName);
+			return;
+		}
+
+		ao.completed += OnLoadOperationComplete;
+		_currentLevelName = levelName;
+	}
+
+
+	public void UnloadLevel(string levelName)
+	{
+		AsyncOperation ao = SceneManager.UnloadSceneAsync(levelName);
+		
+		if(ao == null)
+		{
+			Debug.LogError("[StageManager] unable to unload level " + levelName);
+			return;
+		}
+
+		ao.completed += OnUnloadOperationComplete;
+	}
+}
